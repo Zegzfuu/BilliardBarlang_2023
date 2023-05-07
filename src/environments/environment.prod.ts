4b37020c5e9d09740be332413d222b953182049f
Szegfu_Daniel <szegfudaniel2@gmail.com>
2023-05-07 23:24:06 +0200

    secondcommit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
     apiKey: "AIzaSyDqskZmCg3TYD28dFvcLsRiRFbRjcoQLeA",
-  authDomain: "billiardbarlangfirebasecloud.firebaseapp.com",
-  projectId: "billiardbarlangfirebasecloud",
-  storageBucket: "billiardbarlangfirebasecloud.appspot.com",
-  messagingSenderId: "16757854508",
-  appId: "1:16757854508:web:e250263c43fd7d1b7e5c89",
-  measurementId: "G-6G90LRCMQW"
+    authDomain: "billiardbarlangfirebasecloud.firebaseapp.com",
+    projectId: "billiardbarlangfirebasecloud",
+    storageBucket: "billiardbarlangfirebasecloud.appspot.com",
+    messagingSenderId: "16757854508",
+    appId: "1:16757854508:web:e250263c43fd7d1b7e5c89",
+    measurementId: "G-6G90LRCMQW"
   },
   production: true
 };
